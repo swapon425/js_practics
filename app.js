c06ef899bea8b325a770211a7931019f72d5c873
Swapon Biswas <swaponbiswas700@gmail.com>
2025-10-04 23:29:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-let button=document.createElement("button");
-let input=document.createElement("input");
-button.innerText="click me";
 
-document.querySelector('body').append(input);
 
-document.querySelector('body').append(button);
-button.setAttribute("id","btn");
-input.setAttribute("placeholder","username");
-let btn=document.querySelector("#btn");
-btn.classList.add(btn);
-
-let p=document.createElement("p");
-p.innerHTML="Apna college <b>Delta</b> practice";
-document.querySelector("body").append(p);
+let para1=document.createElement("p");
+para1.innerText="hey i'm red!";
+document.querySelector('body').append(para1);
+para1.classList.add("green");
