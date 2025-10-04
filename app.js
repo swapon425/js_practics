a685dfeaea94de66a7be5aab165524084cf1a078
Swapon Biswas <swaponbiswas700@gmail.com>
2025-10-04 23:30:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ let para1=document.createElement("p");
 para1.innerText="hey i'm red!";
 document.querySelector('body').append(para1);
 para1.classList.add("green");
+
+
+let p1=document.createElement("p");
+p1.innerHTML="Apna college <b>Delta</b> practice";
+document.querySelector("body").append(p1);
+p1.classList.add("green");
